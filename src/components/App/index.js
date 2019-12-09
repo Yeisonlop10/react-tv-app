ef8a8cd7081eea417f97d812b69d7e7a2eb088fb
Yeison Lopez <yeison.lopez1028@gmail.com>
2019-12-09 10:46:03 -0500

    First commit

---
+++ src/components/App/index.js
@@ -0,0 +1,25 @@
+import React, { Component} from 'react';
+import Main from '../Main/index';
+
+import './App.css';
+import 'whatwg-fetch';
+
+
+
+
+class App extends Component {
+  
+
+  render(){
+    return (
+      <div className="App">
+      <header className="App-header">
+        <h1 className="App-title">Tv Series List</h1>
+      </header>
+      <Main />
+    </div>
+    )
+  }
+}
+
+export default App;
